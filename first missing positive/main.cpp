bc556fd7799d28e2cb3a872792f4696c946eb958
Ragini sharma <raginisharma6006845094@gmail.com>
2021-10-17 19:59:51 +0530

    first missing positive

--- first missing positive/main.cpp
+++ first missing positive/main.cpp
@@ -1,9 +1,41 @@
-#include <iostream>
+//function
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        bool one=false;
+        int n=nums.size();
+        //step1
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                one=true;
+            }
+            if(nums[i]<1||nums[i]>n){
+                nums[i]=1;
+            }
+        }
+        //step2
 
-using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
+            if(one==false){
+                return 1;
+            }
+
+
+        for(int i=0;i<n;i++){
+           int  index=abs(nums[i]);
+            nums[index-1]=-abs(nums[index-1]);
+        }
+
+        //step3
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                return i+1;
+            }
+        }
+                return n+1;
+
+
+
+    }
+};
