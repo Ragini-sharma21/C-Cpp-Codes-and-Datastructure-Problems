6d96998a796f5baa44998035e726f2aa66216c22
Ragini sharma <raginisharma6006845094@gmail.com>
2021-12-01 23:32:25 +0530

    truck tour

--- reverse LL c++/main.cpp
+++ reverse LL c++/main.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
 
-using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+     ListNode*prevptr,*currptr,*nextptr;
+        prevptr=NULL;
+        currptr=nextptr=head;
+        while(nextptr!=NULL){
+         nextptr=nextptr->next;
+        currptr->next=prevptr;
+            prevptr=currptr;
+        currptr=nextptr;
+        }
+        head=prevptr;
+        return head;
+    }
+};
--- reverse a string/main.c
+++ reverse a string/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main()
+// function definition of the revstr()
+void revstr(char *str1)
 {
-    printf("Hello world!\n");
-    return 0;
+    // declare variable
+    int i, len, temp;
+    len = strlen(str1); // use strlen() to get the length of str string
+
+    // use for loop to iterate the string
+    for (i = 0; i < len/2; i++)
+    {
+        // temp variable use to temporary hold the string
+        temp = str1[i];
+        str1[i] = str1[len - i - 1];
+        str1[len - i - 1] = temp;
+    }
 }
+
+    int main()
+    {
+        char str[50]; // size of char string
+        printf (" Enter the string: ");
+        gets(str); // use gets() function to take string
+
+        printf (" \n Before reversing the string: %s \n", str);
+
+        // call revstr() function
+        revstr(str);
+        printf (" After reversing the string: %s", str);
+    }
