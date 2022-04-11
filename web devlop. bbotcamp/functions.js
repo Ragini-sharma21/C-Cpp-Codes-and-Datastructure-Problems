286caa32286523968cb684c005e7d22f44af09a5
Ragini sharma <raginisharma6006845094@gmail.com>
2022-04-11 18:54:51 +0530

    c block

---
+++ web devlop. bbotcamp/functions.js
@@ -0,0 +1,21 @@
+console.log("this is function tutorail");
+
+function streek(names,greeting="javascript"){
+    console.log(names+ "good morning");
+    console.log(names+" "+greeting);
+}
+function sum(a,b){
+   let c=a+b;
+    return c;
+}
+let name="ragini";
+let name1="lopa";
+let name2="arpita";
+let greeting="welcome";
+streek(name1,greeting);
+streek(name2);
+
+let e=2;
+let f=3;
+sum(e,f);
+console.log(sum(e,f));
