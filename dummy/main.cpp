05845b77a91b1370407718ba5ed80ebccf0a1401
Ragini sharma <88306424+Ragini-sharma21@users.noreply.github.com>
2023-07-01 18:48:37 +0530

    dsa concept

---
+++ dummy/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[4]={0};
+    for(int i=1;i<=4;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
---
+++ implement n queue in single array/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using namespace std;
+
+class kqueue{
+public:
+    int k;
+    int n;
+    int *front;
+    int *rear;
+    int *next;
+    int *arr;
+    int freespot;
+
+    public:
+    kqueue(int n,int k){
+
+    this->n=n;
+       this->k=k;
+    front=new int[k];
+    rear=new int[k];
+    for(int i=0;i<k;i++){
+        front[i]=-1;
+        rear[i]=-1;
+    }
+    next=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=i+1;
+    }
+    next[n-1]=-1;
+    arr=new int[n];
+    freespot=0;
+
+
+    }
+
+    void enqueue(int data,int qn){
+    if(freespot==-1){
+        cout<<"NO EMPTY SPACE AVAILABLE"<<endl;
+        return;
+
+    }
+        int index=freespot;
+        freespot=next[index];
+
+    if(front[qn-1]==-1){
+        front[qn-1]=index;
+    }
+    else{
+        next[rear[qn-1]]=index;
+    }
+
+    next[index]=-1;
+    rear[qn-1]=index;
+    arr[index]=data;
+    }
+
+    int dequeue(int qn){
+    if(front[qn-1]==-1){
+        cout<<"underflow"<<endl;
+        return -1;
+    }
+    int index=front[qn-1];
+    front[qn-1]=next[index];
+
+    //manage free slots
+    next[index]=freespot;
+    freespot=index;
+    return arr[index];
+    }
+    };
+
+
+
+int main()
+{
+    kqueue q(10,3);
+    q.enqueue(10,1);
+    q.enqueue(15,1);
+    q.enqueue(20,2);
+    q.enqueue(25,1);
+    cout<<q.dequeue(1)<<endl;
+    cout<<q.dequeue(2)<<endl;
+    cout<<q.dequeue(1)<<endl;
+    cout<<q.dequeue(1)<<endl;
+
+    return 0;
+}
